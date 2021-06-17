afb460c077691d65cb8e09fe0c422751349a87a8
류연경 <@DESKTOP-7ICUVGA>
2021-06-17 23:52:56 +0900

    First commit in dev branch

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -2,6 +2,8 @@
 public class MyHello {
 
 	public void sayHello() {
-		System.out.println("Hello!");
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
 	}
 }
